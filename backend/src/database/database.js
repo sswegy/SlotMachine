2082c542e447a9c4e2526934445f2e4286e2f9ee
ivanmatev1 <ivan_matev_@abv.bg>
2024-03-21 17:05:05 +0200

    a

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -4,7 +4,7 @@ const pool = mysql
     .createPool({
         host: "localhost",
         user: "root",
-        password: "Refd36987412",
+        password: "password",
         database: "slot_machine"
     })
     .promise();
