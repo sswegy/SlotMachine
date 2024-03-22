d39d4d0d2559ee40d0d47733fab0aa32ce6d6ebf
ivanmatev1 <ivan_matev_@abv.bg>
2024-03-22 02:53:01 +0200

    b

--- frontend/utility/apiRequests.js
+++ frontend/utility/apiRequests.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://192.168.0.12:5000'; // vseki put qvno go promenqme v CMD "ipconfig"
+const baseUrl = 'http://localhostcheto:5000'; // vseki put qvno go promenqme v CMD "ipconfig"
 
 export const fetchUsers = async () => {
     axios.get(`${baseUrl}/users`)
