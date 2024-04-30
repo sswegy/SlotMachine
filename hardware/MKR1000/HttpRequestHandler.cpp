c5fbda94de2dae82f6b3dc373bbd3d15cb909b92
Viktor Velkov <20viko06@gmail.com>
2024-04-30 12:07:45 +0300

    added uart communication functions for both the arduinos (still unused)"

--- hardware/MKR1000/HttpRequestHandler.cpp
+++ hardware/MKR1000/HttpRequestHandler.cpp
@@ -1,70 +1,69 @@
 #include "HttpRequestHandler.hpp"
 
-
 HttpRequestHandler::HttpRequestHandler(String server_name, const int port, WiFiClient client) : port(port)
 {
-  this->server_name = server_name;
-  this->client = client;
+	this->server_name = server_name;
+	this->client = client;
 }
 
 String HttpRequestHandler::parseResponse()
 {
-  String response;
+	String response;
 
-  bool isBody = false;
-  while (client.connected())
-    if (client.available()) 
-    {
-      String line = client.readStringUntil('\n');
-      if (line == "\r")
-      {
-        isBody = true;
-        break;
-      }
-    }
+	bool isBody = false;
+	while (client.connected())
+		if (client.available())
+		{
+			String line = client.readStringUntil('\n');
+			if (line == "\r")
+			{
+				isBody = true;
+				break;
+			}
+		}
 
-  while (client.connected() && isBody)
-    if (client.available()) 
-    {
-      char c = client.read();
-      response += c;
-    }
-    
-  return response;
+	while (client.connected() && isBody)
+		if (client.available())
+		{
+			char c = client.read();
+			response += c;
+		}
+
+	return response;
 }
 
 String HttpRequestHandler::sendGetRequest(String path, String query)
 {
-  if (client.connect(this->server_name.c_str(), this->port))
-  {
-    Serial.println("Connected to server.");
-    client.println(String("GET ") + path + query + String(" HTTP/1.1"));
-    client.println("Host: " + String(server_name));
-    client.println("Connection: close");
-    client.println();
-  }
-  else
-    Serial.print("Connection to server failed.");
-    
-  return this->parseResponse();
+	if (client.connect(this->server_name.c_str(), this->port))
+	{
+		Serial.println("Connected to server.");
+		client.println(String("GET ") + path + query + String(" HTTP/1.1"));
+		client.println("Host: " + String(server_name));
+		client.println("Connection: close");
+		client.println();
+	}
+	else
+		Serial.print("Connection to server failed.");
+
+	return this->parseResponse();
 }
 
 String HttpRequestHandler::sendPostRequest(String path, String query)
 {
-  if (client.connect(this->server_name.c_str(), this->port))
-  {
-    Serial.println("Connected to server.");
-    client.println(String("POST ") + path + String(" HTTP/1.1"));
-    client.println("Host: " + String(this->server_name));
-    client.println("Connection: close");
-    client.println("Content-Type: text/plain");
-    client.println("Accept: */*");
-    client.println("Content-Length: " + String(query.length()));
-    client.println();
-    client.println(query); 
-  }
-  else
-    Serial.print("Connection to server failed.");
+	if (client.connect(this->server_name.c_str(), this->port))
+	{
+		Serial.println("Connected to server.");
+		client.println(String("POST ") + path + String(" HTTP/1.1"));
+		client.println("Host: " + String(this->server_name));
+		client.println("Connection: close");
+		client.println("Content-Type: text/plain");
+		client.println("Accept: */*");
+		client.println("Content-Length: " + String(query.length()));
+		client.println();
+		client.println(query);
+	}
+	else
+		Serial.print("Connection to server failed.");
 
-  return this->parseResponse();
+	return this->parseResponse();
 }
--- hardware/MKR1000/HttpRequestHandler.hpp
+++ hardware/MKR1000/HttpRequestHandler.hpp
@@ -7,19 +7,19 @@
 #include <WiFi101.h>
 #include <SPI.h>
 
-
 class HttpRequestHandler
 {
-  String server_name;
-  const int port;
-  WiFiClient client;
+	String server_name;
+	const int port;
+	WiFiClient client;
 
-  String parseResponse();
+	String parseResponse();
+
 public:
-  HttpRequestHandler(String server_name, const int port, WiFiClient client);
+	HttpRequestHandler(String server_name, const int port, WiFiClient client);
 
-  String sendGetRequest(String path, String query);
-  String sendPostRequest(String path, String query);
+	String sendGetRequest(String path, String query);
+	String sendPostRequest(String path, String query);
 };
 
 #endif
